9872649684487d30bf1c27e8c931983d592cf34f
Shannon Nolan <shannonnolan17@gmail.com>
2018-02-11 12:09:09 -0500

    Add kue for job queue

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var app = express();
+const kue = require('kue')
+const jobs = kue.createQueue({
+  redis: process.env.REDIS_URL
+})
+
+function newJob (){
+ var job = jobs.create('new_job');
+ job.save();
+}
+
+jobs.process('new_job', function (job, done){
+ console.log("Job", job.id, "is done");
+ done && done();
+})
+
+setInterval(newJob, 3000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(3000, function () {
-    console.log("app running on port.", server.address().port);
-});
+
+
