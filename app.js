c85f08358b7331b150650d147f247384a4003906
Shannon Nolan <shannonnolan17@gmail.com>
2018-02-11 13:50:18 -0500

    Add README

--- app.js
+++ app.js
@@ -12,22 +12,34 @@ var server = app.listen(3000, function () {
     console.log("app running on port.", server.address().port);
 });
 
-// function newJob (){
-//  var job = jobs.create('new_job');
-//  job.save();
-// }
+function newJob (){
+ var job = jobs.create('new_job');
+ job.save();
+}
 
-// jobs.process('new_job', function (job, done){
-//  console.log("Job", job.id, "is done");
-//  done && done();
-// })
+jobs.process('new_job', function (job, done){
+ console.log("Job", job.id, "is done");
+ done && done();
+})
 
-// setInterval(newJob, 3000);
+setInterval(newJob, 3000);
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-routes(app);
+var appRouter = function (app) {
+  app.get("/", function(req, res) {
+    // res.status(200).send("Welcome to our restful API");
+
+  request('https://www.google.com', function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      console.log(html);
+    }
+  });
+
+    // res.status(200).send(data);
+  });
+  };
 
 
 
