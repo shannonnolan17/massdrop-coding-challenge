a253f38bfac730773317b626e9a1f84fb800dc93
Shannon Nolan <shannonnolan17@gmail.com>
2018-02-15 20:18:06 -0500

    Added routes

--- app/controllers/website.controller.js
+++ app/controllers/website.controller.js
@@ -4,14 +4,13 @@ const cheerio = require('cheerio');
 const request = require('request');
 var Website = require('../models/website.model.js');
 
-
 exports.create = function(req, res) {
     if(!req.body.content) {
       res.status(400).send({message: "Website cannot be empty"});
     }
 
-    var site = {content: req.body.content};
-    //take the request and add to job queue
+//     var site = {content: req.body.content};
+//     //take the request and add to job queue
 
 if (process.env.NODE_ENV === 'production') {
   redisConfig = {
@@ -27,15 +26,11 @@ if (process.env.NODE_ENV === 'production') {
 
 const queue = require('kue').createQueue(redisConfig);
 
-
-queue.on('ready', () => {
-  console.info('Queue is ready!');
-});
-
 queue.on('error', (err) => {
   console.error('There was an error!');
   console.error(err);
 });
+
 function createJob(data, done) {
   queue.create('newJob', data)
     .priority('critical')
@@ -54,40 +49,38 @@ function createJob(data, done) {
 }
 
 queue.process('newJob', (job, done) => {
-  // This is the data we sent into the #create() function call earlier
-  // We're setting it to a constant here so we can do some guarding against accidental writes
   const data = job.data;
     request({
-      method: 'GET',
-      url: data
+      method: 'POST',
+      url: req.body.content
     }, function(err, response, body) {
       if (err) return console.error(err);
       res.send(body);
+        website.save(body)
+        var website = new Website({content: response});
+
+          // if(err) {
+          //   console.log(err);
+          //   res.status(500).send({message: "Some error occurred while creating the Website"});
+          // }
+
 
     });
-  //... do other stuff with the data.
+
 });
-module.exports = {
-  create: (data, done) => {
-    createJob(data, done);
-  }
-};
+// module.exports = {
+//   create: (data, done) => {
+//     createJob(data, done);
+//   }
+// };
 
 
 
 
     //take the website from request and scrape the html
       // //save the result of scrape and jobId to database
-      //   var website = new Website({content: req.body.content});
 
       //   //save website to database
-      //   website.save(function(err, data) {
-      //     res.send({message: "Your id is " + website.id})
-      //     if(err) { //error handling
-      //       console.log(err);
-      //       res.status(500).send({message: "Some error occurred while creating the Website"});
-      //     }
-      //   });
 
 };
 
--- server.js
+++ server.js
@@ -1,64 +1,31 @@
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var kue = require('kue');
-// kue.app.listen(3000);
+var express = require('express');
+var bodyParser = require('body-parser');
+var kue = require('kue');
+kue.app.listen(3000);
 
-// var app = express();
+var app = express();
 
-// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 
-// var dbConfig = require('./config/database.config.js');
+var dbConfig = require('./config/database.config.js');
 
+var mongoose = require('mongoose');
 
+const db = mongoose.connect('mongodb://localhost:27017/massdrop');
 
-var app = require('express')();
-var responseTime = require('response-time')
-var axios = require('axios');
-var redis = require('redis');
-
-// create a new redis client and connect to our local redis instance
-var client = redis.createClient();
-
-// if an error occurs, print it to the console
-client.on('error', function (err) {
-    console.log("Error " + err);
-});
-
-app.set('port', (process.env.PORT || 5000));
-
-// set up the response-time middleware
-app.use(responseTime());
-
-// if a user visits /api/facebook, return the total number of stars 'facebook'
-// has across all it's public repositories on GitHub
-app.get('/', function(req, res) {
+mongoose.connection.once('open', function() {
+    console.log("Successfully connected to the database");
+})
+mongoose.connection.on('error', function() {
+    console.log('Could not connect to the database. Exiting now...');
 
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Server listening on port: ', app.get('port'));
-});
-
-// var mongoose = require('mongoose');
-
-// const db = mongoose.connect('mongodb://localhost:27017/massdrop');
-
-// mongoose.connection.once('open', function() {
-//     console.log("Successfully connected to the database");
-// })
-// mongoose.connection.on('error', function() {
-//     console.log('Could not connect to the database. Exiting now...');
-
-// });
-
-// app.get('/', function(req, res){
+// app.get('/active', function(req, res){
 //     res.json({"message": "Welcome to the job queue"});
 // });
 
-// require('./app/routes/website.routes.js')(app);
+require('./app/routes/website.routes.js')(app);
 
-// app.listen(3000, function(){
-//     console.log("Server is listening on port 3000");
-// });
