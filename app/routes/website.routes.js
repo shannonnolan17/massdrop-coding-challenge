6631f07bedba854c5f53f52ce7c431cb6a06c0ca
Shannon Nolan <shannonnolan17@gmail.com>
2018-02-18 20:33:04 -0500

    Update controller

--- app/routes/website.routes.js
+++ app/routes/website.routes.js
@@ -4,22 +4,22 @@ var router = require('express').Router();
   // Create a new website
   const websites = require('../controllers/website.controller.js');
   router.post('/api', (req, res, next) => {
-    const content = req.body.content;
-    websites.create(content, (err) => {
-      if (err) {
-        return res.json({
-          error: err,
-          success: false,
-          message: 'Could not create content',
-        });
-      } else {
-        return res.json({
-          error: null,
-          success: true,
-          message: 'Created a website!', content
-        });
-      }
-    })
+    // const content = req.body.content;
+    // websites.create(content, (err) => {
+    //   if (err) {
+    //     return res.json({
+    //       error: err,
+    //       success: false,
+    //       message: 'Could not create content',
+    //     });
+    //   } else {
+    //     return res.json({
+    //       error: null,
+    //       success: true,
+    //       message: 'Created a website!', content
+    //     });
+    //   }
+    // })
   });
 
     // find website id
