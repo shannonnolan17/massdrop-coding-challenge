7fdc3e6e5438e41d82dd88da31fa30957f3c6528
Shannon Nolan <shannonnolan17@gmail.com>
2018-02-19 19:21:05 -0500

    Attempt to fix api call issue

--- app/controllers/website.controller.js
+++ app/controllers/website.controller.js
@@ -1,109 +1,109 @@
-const rp = require('request-promise');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const request = require('request');
-const Website = require('../models/website.model.js');
+// const rp = require('request-promise');
+// const axios = require('axios');
+// const cheerio = require('cheerio');
+// const request = require('request');
+// const Website = require('../models/website.model.js');
 
-    let redisConfig;
-    if (process.env.NODE_ENV === 'production') {
-      redisConfig = {
-        redis: {
-          port: process.env.REDIS_PORT,
-          host: process.env.REDIS_HOST,
-          auth: process.env.REDIS_PASS,
-          options: {
-            no_ready_check: false
-          }
-        }
-      };
-    } else {
-      redisConfig = {};
-    }
+//     let redisConfig;
+//     if (process.env.NODE_ENV === 'production') {
+//       redisConfig = {
+//         redis: {
+//           port: process.env.REDIS_PORT,
+//           host: process.env.REDIS_HOST,
+//           auth: process.env.REDIS_PASS,
+//           options: {
+//             no_ready_check: false
+//           }
+//         }
+//       };
+//     } else {
+//       redisConfig = {};
+//     }
 
-exports.create = function(req, res) {
-    if(!req.body.content) {
-      res.status(400).send({message: "Website cannot be empty"});
-    }
+// exports.create = function(req, res) {
+//     if(!req.body.content) {
+//       res.status(400).send({message: "Website cannot be empty"});
+//     }
 
-    const queue = require('kue').createQueue(redisConfig);
+//     const queue = require('kue').createQueue(redisConfig);
 
-    queue.on('error', (err) => {
-      console.error('There was an error!');
-      console.error(err);
-    });
+//     queue.on('error', (err) => {
+//       console.error('There was an error!');
+//       console.error(err);
+//     });
 
-    function createJob(data, done) {
-      queue.create('newJob', data)
-        .priority('critical')
-        .on('completed', (result) => {
-        console.log('Job is complete:', result);
-        })
-        .on('failed', (errorMessage) => {
-        console.log('Job could not process');
-        })
-        .on('progress', (progress, data) => {
-        console.log('\r  job #' + job.id + ' ' + progress + '% complete: ', data);
-        })
-        .save((err) => {
-          if (err) {
-            console.error(err);
-            done(err);
-          }
-          if (!err) {
-            res.send({
-            message: 'Your job was created. Here is the job id: ' + job.id,
-            success: true
-          });
-          }
-        });
-    }
+//     function createJob(data, done) {
+//       queue.create('newJob', data)
+//         .priority('critical')
+//         .on('completed', (result) => {
+//         console.log('Job is complete:', result);
+//         })
+//         .on('failed', (errorMessage) => {
+//         console.log('Job could not process');
+//         })
+//         .on('progress', (progress, data) => {
+//         console.log('\r  job #' + job.id + ' ' + progress + '% complete: ', data);
+//         })
+//         .save((err) => {
+//           if (err) {
+//             console.error(err);
+//             done(err);
+//           }
+//           if (!err) {
+//             res.send({
+//             message: 'Your job was created. Here is the job id: ' + job.id,
+//             success: true
+//           });
+//           }
+//         });
+//     }
 
-    queue.process('newJob', (job, done) => {
-      const data = job.data;
-        request({
-          method: 'GET',
-          url: req.body.content
-        }, function(err, response, body) {
-          if (!err) return console.error(err);
-          res.send(body);
-            website.save(body)
-            var website = new Website({content: response});
+//     queue.process('newJob', (job, done) => {
+//       const data = job.data;
+//         request({
+//           method: 'GET',
+//           url: req.body.content
+//         }, function(err, response, body) {
+//           if (!err) return console.error(err);
+//           res.send(body);
+//             website.save(body)
+//             var website = new Website({content: response});
 
-              // if(err) {
-              //   console.log(err);
-              //   res.status(500).send({message: "Some error occurred while creating the Website"});
-              // }
+//               // if(err) {
+//               //   console.log(err);
+//               //   res.status(500).send({message: "Some error occurred while creating the Website"});
+//               // }
 
 
-        });
+//         });
 
-});
-// module.exports = {
-//   create: (data, done) => {
-//     createJob(data, done);
-//   }
+// });
+// // module.exports = {
+// //   create: (data, done) => {
+// //     createJob(data, done);
+// //   }
+// // };
+
+
+
+
+//     //take the website from request and scrape the html
+//       // //save the result of scrape and jobId to database
+
+//       //   //save website to database
+
 // };
 
 
 
+// exports.findOne = function(req, res) {
+//     // Find a single Website with an id
+//     Website.findById(req.params.websiteId, function(err, data) {
+//       if(err) {
+//         res.status(500).send({message: "Could not retrieve website status with id " + req.params.websiteId});
+//       } else {
+//         res.send(data);
+//       }
+//     });
 
-    //take the website from request and scrape the html
-      // //save the result of scrape and jobId to database
-
-      //   //save website to database
-
-};
-
-
-
-exports.findOne = function(req, res) {
-    // Find a single Website with an id
-    Website.findById(req.params.websiteId, function(err, data) {
-      if(err) {
-        res.status(500).send({message: "Could not retrieve website status with id " + req.params.websiteId});
-      } else {
-        res.send(data);
-      }
-    });
-
-};
+// };
--- app/routes/website.routes.js
+++ app/routes/website.routes.js
@@ -1,29 +1,90 @@
 'use strict';
+const rp = require('request-promise');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const request = require('request');
+const Website = require('../models/website.model.js');
+
 
 var router = require('express').Router();
   // Create a new website
-  const websites = require('../controllers/website.controller.js');
-  router.post('/api', (req, res, next) => {
-    // const content = req.body.content;
-    // websites.create(content, (err) => {
-    //   if (err) {
-    //     return res.json({
-    //       error: err,
-    //       success: false,
-    //       message: 'Could not create content',
+  // const websites = require('../controllers/website.controller.js');
+  router.post('/api', function (req, res) {
+   // let redisConfig;
+   //  if (process.env.NODE_ENV === 'production') {
+   //    redisConfig = {
+   //      redis: {
+   //        port: process.env.REDIS_PORT,
+   //        host: process.env.REDIS_HOST,
+   //        auth: process.env.REDIS_PASS,
+   //        options: {
+   //          no_ready_check: false
+   //        }
+   //      }
+   //    };
+   //  } else {
+   //    redisConfig = {};
+   //  }
+    if(!req.body.content) {
+      res.status(400).send({message: "Website cannot be empty"});
+    }
+
+    const queue = require('kue').createQueue(redisConfig);
+
+    queue.on('error', (err) => {
+      console.error('There was an error!');
+      console.error(err);
+    });
+
+    function createJob(data, done) {
+      queue.create('newJob', data)
+        .on('completed', (result) => {
+        res.send('Job is complete:', result);
+        })
+        .on('failed', (errorMessage) => {
+        res.send('Job could not process');
+        })
+        .on('progress', (progress, data) => {
+        res.send('\r  job #' + job.id + ' ' + progress + '% complete: ', data);
+        })
+        .save((err) => {
+          if (err) {
+            console.error(err);
+            done(err);
+          }
+          if (!err) {
+            res.send({
+            message: 'Your job was created. Here is the job id: ' + job.id,
+            success: true
+          });
+          }
+        });
+    }
+
+    // queue.process('newJob', (job, done) => {
+    //   const data = job.data;
+    //     request({
+    //       method: 'GET',
+    //       url: req.body.content
+    //     }, function(err, response, body) {
+    //       if (!err) return console.error(err);
+    //       res.send(body);
+    //         website.save(body)
+    //         var website = new Website({content: response});
+
+    //           // if(err) {
+    //           //   console.log(err);
+    //           //   res.status(500).send({message: "Some error occurred while creating the Website"});
+    //           // }
+
+
     //     });
-    //   } else {
-    //     return res.json({
-    //       error: null,
-    //       success: true,
-    //       message: 'Created a website!', content
-    //     });
-    //   }
-    // })
-  });
+
+    // });
+  })
 
     // find website id
-    router.get('/api/:websiteId', websites.findOne);
+    // router.get('/api/:websiteId', websites.findOne);
 
 
 module.exports = router
